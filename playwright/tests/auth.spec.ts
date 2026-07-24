c51eacf0671a0dfd2db0578b3abd1440239b4c2d
AnnieK7H <anjahalatyan@gmail.com>
2026-07-24 22:02:29 +0300

    test(auth): add localStorage session evaluation to verify successful login

--- playwright/tests/auth.spec.ts
+++ playwright/tests/auth.spec.ts
@@ -1,17 +1,20 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 import {LoginPage} from "../pages/loginPage";
 
 test.describe('Login page: Authentication', () => {
     let loginPage: LoginPage
 
-    test.beforeEach(async ({page}) => {
+    test.beforeEach(async ({page}: {page: Page}) => {
         loginPage = new LoginPage(page)
         await loginPage.navigate()
     })
 
-    test('Successful login', async ({page}) => {
+    test('Successful login', async ({page}: {page: Page}) => {
         await loginPage.login()
         const sidebar = page.getByTestId('sidebar-menu')
         await expect(sidebar).toBeVisible()
+
+        const storageUsername = await page.evaluate(() => localStorage.getItem('username'))
+        expect(storageUsername).toBe(process.env.ADMIN_DEMO_USERNAME)
     })
 })
